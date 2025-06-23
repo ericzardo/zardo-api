26bed044e80a2b23adb2c2be4b869a20122b74b3
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-06-23 17:44:20 -0300

    change server port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ fastify.register(scrapeRoutes);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    await fastify.listen({ port: Number(PORT), host: '0.0.0.0' });
     console.log('🚀 Server ready at http://localhost:3000');
   } catch (err) {
     fastify.log.error(err);
