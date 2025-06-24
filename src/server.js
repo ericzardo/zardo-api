97269b4b5c7ba4b660910f3351d9210858e07fcc
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-06-24 11:28:44 -0300

    fix ready log

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ fastify.register(scrapeRoutes);
 const start = async () => {
   try {
     await fastify.listen({ port: Number(process.env.PORT), host: '0.0.0.0' });
-    console.log('🚀 Server ready at http://localhost:3000');
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
