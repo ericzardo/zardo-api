48e2323fc054eeb892d223388cc3288caad3a882
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-07-12 16:38:37 -0300

    register cors on fastify

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import 'dotenv/config';
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
 import routes from './routes';
 
 async function main() {
   const app = Fastify({ logger: true });
 
   await app.register(routes);
+  await app.register(cors, {
+  origin: (origin, cb) => {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'https://dashboard.zardo.dev',
+    ];
+    if (!origin) return cb(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Not allowed by CORS"), origin);
+    }
+  },
+});
 
   try {
     await app.listen({ port: parseInt(process.env.PORT || '9999', 10), host: '0.0.0.0' });
