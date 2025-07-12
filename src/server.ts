8888f324e8e3e4884326699161884d56d2069a43
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-07-12 18:33:22 -0300

    add methods & headers cors

--- src/server.ts
+++ src/server.ts
@@ -8,20 +8,22 @@ async function main() {
 
   await app.register(routes);
   await app.register(cors, {
-  origin: (origin, cb) => {
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'https://dashboard.zardo.dev',
-    ];
-    if (!origin) return cb(null, true);
+    origin: (origin, cb) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://dashboard.zardo.dev',
+      ];
+      if (!origin) return cb(null, true);
 
-    if (allowedOrigins.includes(origin)) {
-      cb(null, true);
-    } else {
-      cb(new Error("Not allowed by CORS"), origin);
-    }
-  },
-});
+      if (allowedOrigins.includes(origin)) {
+        cb(null, true);
+      } else {
+        cb(new Error("Not allowed by CORS"), origin);
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+  });
 
   try {
     await app.listen({ port: parseInt(process.env.PORT || '9999', 10), host: '0.0.0.0' });
